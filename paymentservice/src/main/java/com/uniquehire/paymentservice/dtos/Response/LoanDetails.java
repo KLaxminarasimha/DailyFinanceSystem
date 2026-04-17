7d272c6147c5db5fdec34c92ec5546d4d8b1cb86
rishithavarala0208 <rishithavarala0208@gmail.com>
2026-04-17 17:35:28 +0530

    make change in PaymentService

--- paymentservice/src/main/java/com/uniquehire/paymentservice/dtos/Response/LoanDetails.java
+++ paymentservice/src/main/java/com/uniquehire/paymentservice/dtos/Response/LoanDetails.java
@@ -1,4 +1,22 @@
 package com.uniquehire.paymentservice.dtos.Response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoanDetails {
+    private Long loanId;
+    private String loanStatus;
+    private BigDecimal dailyEmi;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private BigDecimal amountPaid;
+    private BigDecimal totalFine;
+    private BigDecimal totalAmount;
 }
